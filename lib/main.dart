e08d519a88d3ddc4778bc2d271eaf96b3e6bdabb
aryaman <shrivastavaaryaman0207@gmail.com>
2023-01-29 22:50:28 +0530

    ScreenUtils package update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'firebase_options.dart';
 import 'views/routes/pages.dart';
@@ -16,17 +17,16 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ScreenUtilInit(
+      builder: (BuildContext context, Widget? child) => GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: AppPages.INITIAL,
+        getPages: AppPages.routes,
       ),
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-      // home: const Center(
-      //   child: Text("Project Started"),
-      // ),
     );
   }
 }
--- lib/pages/welcome/view.dart
+++ lib/pages/welcome/view.dart
@@ -10,49 +10,49 @@ class WelcomePage extends GetView<WelcomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Obx(() => SizedBox(
-              width: 360.w,
-              height: 780.w,
-              child: Stack(
-                alignment: Alignment.bottomCenter,
-                children: [
-                  PageView(
-                    scrollDirection: Axis.horizontal,
-                    reverse: false,
-                    onPageChanged: (index) {},
-                    controller: PageController(
-                        initialPage: 0, keepPage: false, viewportFraction: 1),
-                    pageSnapping: true,
-                    physics: const ClampingScrollPhysics(),
-                    children: [
-                      Container(
-                        width: double.infinity,
-                        height: double.infinity,
-                        decoration: const BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage("assets/images/banner1.png"),
-                                fit: BoxFit.fill)),
-                      ),
-                      Container(
-                        width: double.infinity,
-                        height: double.infinity,
-                        decoration: const BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage("assets/images/banner1.png"),
-                                fit: BoxFit.fill)),
-                      ),
-                      Container(
-                        width: double.infinity,
-                        height: double.infinity,
-                        decoration: const BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage("assets/images/banner1.png"),
-                                fit: BoxFit.fill)),
-                      ),
-                    ],
-                  )
-                ],
+        body: SizedBox(
+      width: 360.w,
+      height: 780.w,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          PageView(
+            scrollDirection: Axis.horizontal,
+            reverse: false,
+            onPageChanged: (index) {},
+            controller: PageController(
+                initialPage: 0, keepPage: false, viewportFraction: 1),
+            pageSnapping: true,
+            physics: const ClampingScrollPhysics(),
+            children: [
+              Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/banner1.png"),
+                        fit: BoxFit.fill)),
               ),
-            )));
+              Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/banner1.png"),
+                        fit: BoxFit.fill)),
+              ),
+              Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/banner1.png"),
+                        fit: BoxFit.fill)),
+              ),
+            ],
+          )
+        ],
+      ),
+    ));
   }
 }
